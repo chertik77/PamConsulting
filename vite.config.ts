75e173f75ab6820ed47875595b9938a47007bca8
chertik77 <babychdenya@gmail.com>
2024-05-15 14:27:59 +0100

    chore: init

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { glob } from "glob";
+import { defineConfig } from "vite";
+import injectHTML from "vite-plugin-html-inject";
+
+export default defineConfig({
+  root: "src",
+  build: {
+    rollupOptions: {
+      input: glob.sync("./src/*.html"),
+    },
+    outDir: "../dist",
+  },
+  plugins: [injectHTML()],
+});
